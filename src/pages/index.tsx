06cd6c570fbf40d149897fd6b9d8c6382c6692d3
Kerem Ersoy <azimetkeremersoy@gmail.com>
2025-05-20 18:24:44 +0300

    update homepage content to enhance clarity

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import MaxWidthWrapper from "@/components/max-width-wrapper";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <MaxWidthWrapper className="space-y-24">
-        <h1 className="px-12 text-center text-8xl font-bold tracking-wide">kku sınav takvim</h1>
+        <section className="py-20 text-center">
+          <h1 className="text-5xl font-bold tracking-tight text-primary sm:text-6xl md:text-7xl">
+            Kırıkkale Üniversitesi Sınav Takvimi Oluşturucu
+          </h1>
+          <p className="mt-6 text-lg leading-8 text-muted-foreground">
+            Kırıkkale Üniversitesi akademisyenlerinin sınav takvimini yönetmesi için tasarlandı.
+          </p>
+        </section>
       </MaxWidthWrapper>
     </>
   );
